bbcce8edee85e104ffc06c2dbf7fdcccfec74f9d
Patrick Zimbrod <patrick.zimbrod@gmail.com>
2023-07-24 15:48:42 +0200

    include capillary stress tensor

--- src/weak_form.py
+++ src/weak_form.py
@@ -1,8 +1,10 @@
+from numpy.linalg import norm
 from dolfinx.fem import form
 from dolfinx.fem import Function
 from ufl import (FacetNormal, FiniteElement, Identity, Measure, TestFunction, TrialFunction, VectorElement,
                  MixedElement,as_vector, div, dot, ds, dx, inner, lhs, grad, nabla_grad, rhs, sym, variable,
-                 jump,avg, Form)
+                 jump,avg, Form, Identity, outer, Dx)
+from ufl.algebra import Power, Sum
 
 def upwind(velocity: Function,n: FacetNormal, trial: TrialFunction) -> Form:
     vel_n = 0.5*(dot(velocity, n) + abs(dot(velocity, n)))
@@ -17,6 +19,26 @@ def HLLE(velocity: Function,n: FacetNormal, trial: TrialFunction) -> Form:
     v_min = min(min(velocity),0)
     return v_max/(v_max-v_min) * dot(velocity('+'),n('+'))*trial('+') - \
           v_min * dot(velocity('-'),n('+'))*trial('-') - v_max * v_min * jump(trial)
+
+def interface_marker(alpha: Function) -> Form:
+    dim = 2
+    return Power(Sum(
+        *[Power(Dx(alpha,i),2) for i in range(0,dim)]),
+        0.5
+    )
+
+def interface_normal(alpha: Function) -> Form:
+    mag_alpha = interface_marker(alpha=alpha)
+    return 1/mag_alpha * grad(alpha)
+
+def capillary_stress(alpha: Function,n: FacetNormal) -> Form:
+    (dim,) = n.ufl_shape
+    I = Identity(dim=dim)
+    n = interface_normal(alpha=alpha)
+    return interface_marker(alpha=alpha) * (I-outer(n,n))
+
+def DG_gradient(test: TestFunction, fn: Function, u: Function, n: FacetNormal, flux_function):
+    return inner(fn,div(test*u))*dx - jump(test)*flux_function(u,n,fn)*ds
 
 def generate_weak_form(mesh,trialFunctions,testFunctions,functions,flux_function):
     test_as, test_al, test_ag, test_T, test_p, test_u = testFunctions["test_alpha_solid"], testFunctions["test_alpha_liquid"], \
@@ -37,8 +59,20 @@ def generate_weak_form(mesh,trialFunctions,testFunctions,functions,flux_function
     F += inner(test_al,dalpha_liquid)*dx
     F += inner(test_ag,dalpha_gas)*dx
     F += inner(test_p,dp)*dx
+    F += inner(test_u,du)*dx
     F += inner(test_T,dT)*dx
 
+    # VoF
+    F += DG_gradient(test=test_as,fn=alpha_solid,u=u,n=n,flux_function=flux_function)
+    F += DG_gradient(test=test_al,fn=alpha_liquid,u=u,n=n,flux_function=flux_function)
+    F += DG_gradient(test=test_ag,fn=alpha_gas,u=u,n=n,flux_function=flux_function)
+    # Pressure
+    F += -inner(p,div(test_u))*dx
+    # Continuity
+    F += -inner(div(u),test_p)*dx
+    # Velocity
+    F += inner(test_u,div(capillary_stress(alpha=alpha_liquid,n=n)))*dx
+    # Temperature
 
     A, l = lhs(F), rhs(F)
     return A, l
