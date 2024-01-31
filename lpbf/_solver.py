5fc0a81bf54461c677d0fd780893f3e2c8d16345
Patrick Zimbrod <patrick.zimbrod@gmail.com>
2024-01-31 14:46:17 +0100

    change solver config

--- lpbf/_solver.py
+++ lpbf/_solver.py
@@ -11,11 +11,24 @@ class Solver:
                                      bcs=self.bcs)
 
     def _assemble_solver(self, solver_parameters: dict | None) -> None:
-        if solver_parameters is None:
-            solver_parameters={'snes_type': 'newtonls',
-                            'ksp_type': 'preonly',
-                            'pc_type': 'lu'}
+        
         self.solver = nls.petsc.NewtonSolver(self.mesh.comm,self.prob)
+        self.solver.convergence_criterion = "residual"
+        self.solver.rtol = 1e-6
+        self.solver.report = True
+        if solver_parameters is None:
+            ksp = self.solver.krylov_solver
+            opts = PETSc.Options()  # type: ignore
+            option_prefix = ksp.getOptionsPrefix()
+            opts[f"{option_prefix}ksp_type"] = "preonly"
+            opts[f"{option_prefix}pc_type"] = "lu"
+            sys = PETSc.Sys()  # type: ignore
+            # For factorisation prefer MUMPS, then superlu_dist, then default.
+            if sys.hasExternalPackage("mumps"):
+                opts[f"{option_prefix}pc_factor_mat_solver_type"] = "mumps"
+            elif sys.hasExternalPackage("superlu_dist"):
+                opts[f"{option_prefix}pc_factor_mat_solver_type"] = "superlu_dist"
+            ksp.setFromOptions()
 
     def solve(self):
         n, converged = self.solver.solve(self.solution.next)
