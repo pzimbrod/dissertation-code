0028e710e957c6f2ab450383480d4377667d15a7
Patrick Zimbrod <patrick.zimbrod@gmail.com>
2024-06-11 10:54:55 +0200

    create docstrings for output methods

--- powder-bed-fusion/MultiphysicsModel/Output.py
+++ powder-bed-fusion/MultiphysicsModel/Output.py
@@ -4,6 +4,21 @@ from .FEData import AbstractFEData
 
 class Output:
     def __init__(self, path: str, mesh: Mesh, fe_data: AbstractFEData) -> None:
+        """
+        Initializes an instance of the Output class.
+
+        Parameters:
+            path (str): The path where the output files will be saved.
+            mesh (Mesh): The mesh object representing the computational domain.
+            fe_data (AbstractFEData): The Finite Element data object.
+
+        Returns:
+            None
+
+        This function initializes the Output class by creating VTK files for each field in the fe_data object. 
+        The files are saved in the specified path with the field name appended to the path. The mesh is written 
+        to each VTK file using the write_mesh method of the VTKFile class.
+        """
         self.files = {}
         functions = fe_data.solution
         for field in functions.keys():
@@ -17,6 +32,16 @@ class Output:
     
 
     def write(self, fe_data: AbstractFEData, time:float) -> None:
+        """
+        Write the solution data to the output files.
+
+        Parameters:
+            fe_data (AbstractFEData): The Finite Element data.
+            time (float): The current time.
+
+        Returns:
+            None
+        """
         functions = fe_data.solution
         for (field, function) in functions.items():
             with self.files[field] as file:
